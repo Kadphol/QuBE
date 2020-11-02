0a9c1a53d7568f78a47d3667a80d952cb9a7d4b2
DESKTOP-J1EOFF5\surface <tenten25420@gmail.com>
2020-11-02 16:36:59 +0700

    Add login component

---
+++ frontend/src/pages/App/Login.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Login = () => {
+    state = {
+        loginStatus: false
+    }
+    return (
+        <div>
+            <a style={this.state.loginStatus?"display:none":""} href="http://127.0.0.1:80/auth/facebook">
+                <button> Login </button>
+            </a>
+        </div>
+    )
+}
+
+export default Login;
