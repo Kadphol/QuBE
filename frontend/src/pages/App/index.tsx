8548cb8fd4b0df5693136fc16af3449b11cc992c
Kadphol <kadphol.len@outlook.co.th>
2020-11-02 18:01:23 +0700

    Add NavigationBar

--- frontend/src/pages/App/index.tsx
+++ frontend/src/pages/App/index.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from '../../assets/logo.svg';
+import { Route } from 'react-router-dom'
+
+import NavigationBar from '../../components/NavigationBar/NavigationBar';
+import Home from '../Home';
+
 import '../../styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Navigation">
+        <NavigationBar />
+      </div>
+      <div>
+        <Route exact path= "/" component={Home} />
+        <Route path="/explore" />
+        <Route path="/playground" />
+        <Route path="/challenge" />
+      </div> 
     </div>
   );
 }
