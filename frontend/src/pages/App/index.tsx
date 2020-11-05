a0e4daa751a217ee860f1071854e33d65f552cf3
DESKTOP-J1EOFF5\surface <tenten25420@gmail.com>
2020-11-05 20:18:12 +0700

    route explore

--- frontend/src/pages/App/index.tsx
+++ frontend/src/pages/App/index.tsx
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom'
 
 import NavigationBar from '../../components/NavigationBar/NavigationBar';
 import Home from '../Home';
+import Explore from '../Explore';
 
 import '../../styles/App.css';
 
@@ -15,7 +16,7 @@ function App() {
       </div>
       <div>
         <Route exact path= "/" component={Home} />
-        <Route path="/explore" />
+        <Route path="/explore" component={Explore}/>
         <Route path="/playground" />
         <Route path="/challenge" />
       </div> 
