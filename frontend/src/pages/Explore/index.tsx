1b0f4ea90586218a9fdec0298ff0d1316c7cc148
DESKTOP-J1EOFF5\surface <tenten25420@gmail.com>
2020-11-05 20:17:53 +0700

    layout explore

---
+++ frontend/src/pages/Explore/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import intro from '../../assets/intro.png';
+class Explore extends React.Component {
+  render() {
+    return (
+      <div>
+        <img src={intro} alt="intro"/>
+      </div>
+    );
+  }
+}
+
+export default Explore;
