08f6e00f69bb5ae283d85f55d796e4a01e8c0f0d
DESKTOP-J1EOFF5\surface <tenten25420@gmail.com>
2020-11-09 00:24:03 +0700

    Move out login button

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import LoginButton from '../App/Login';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
         Home
-        <br/>
-        <LoginButton/>
       </div>
     );
   }
