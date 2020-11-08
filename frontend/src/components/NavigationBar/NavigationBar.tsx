1e0f654c51412bd6f6921ed5381eafea2a89cb38
DESKTOP-J1EOFF5\surface <tenten25420@gmail.com>
2020-11-09 00:25:30 +0700

    Add profile component

--- frontend/src/components/NavigationBar/NavigationBar.tsx
+++ frontend/src/components/NavigationBar/NavigationBar.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar,Nav } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 
+import Profile from './Profile'
 import './NavigationBar.css';
+import {Iuser} from '../../type.modal'
+import LoginButton from './LoginButton'
 
-class NavigationBar extends React.Component {
+class NavigationBar extends React.Component<Iuser> {
+
+  constructor(props:Iuser) {
+    super(props);
+}
+  
   render() {
     return (
       <React.Fragment>
@@ -13,9 +21,12 @@ class NavigationBar extends React.Component {
           <Navbar.Collapse>
             <Nav className="justify-content-end ml-auto">
               <NavLink exact to="/" className="nav-link">เริ่มต้นกับควอนตัม</NavLink>
-              <NavLink to="/explore" className="nav-link">ผจญภัยในโลกควอนตัม</NavLink>
+              <NavLink to="/explore" className="nav-link" >ผจญภัยในโลกควอนตัม</NavLink>
               <NavLink to="/playground" className="nav-link">สนามฝึกซ้อมควอนตัม</NavLink>
               <NavLink to="/challenge" className="nav-link">ท้าทายกับควอนตัม</NavLink>
+              {this.props.loginStatus
+              ?<Profile name={this.props.name} loginStatus={this.props.loginStatus} image={this.props.image}/>
+              :<LoginButton/>}
             </Nav>
           </Navbar.Collapse>
           
