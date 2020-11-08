abfaa1f6ca43d17fe240196fe6d85553db2f448a
DESKTOP-J1EOFF5\surface <tenten25420@gmail.com>
2020-11-09 00:24:55 +0700

    Add state and fetch session

--- frontend/src/pages/App/index.tsx
+++ frontend/src/pages/App/index.tsx
@@ -1,25 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route } from 'react-router-dom'
+import axios, { AxiosRequestConfig, AxiosInstance} from 'axios'
 
 import NavigationBar from '../../components/NavigationBar/NavigationBar';
 import Home from '../Home';
 import Explore from '../Explore';
 
 import '../../styles/App.css';
+import { Iuser } from '../../type.modal'
 
 function App() {
-  
+
+  const [user, setUser] = useState<Iuser>({
+    loginStatus: false
+  })
+
+  const config: AxiosRequestConfig = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    withCredentials: true
+  }
+
+  const instance: AxiosInstance = axios.create(config)
+
+  instance.get('http://localhost/fetch')
+    .then(res => {
+      if (res.data) {
+        setUser({
+          loginStatus: true,
+          name: res.data.name,
+          image: res.data.image,
+          progress: res.data.progress
+        });
+      }
+    })
+
   return (
     <div className="App">
       <div className="Navigation">
-        <NavigationBar />
+        <NavigationBar name={user.name} loginStatus={user.loginStatus} image={user.image} />
       </div>
       <div>
-        <Route exact path= "/" component={Home} />
-        <Route path="/explore" component={Explore}/>
+        <Route exact path="/" component={Home} />
+        <Route path="/explore" component={Explore} />
         <Route path="/playground" />
         <Route path="/challenge" />
-      </div> 
+      </div>
     </div>
   );
 }
