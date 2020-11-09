45f28dd981dc13dc59d7f9ef2abbc93c6d085434
Kadphol <kadphol.len@outlook.co.th>
2020-11-09 23:18:18 +0700

    Add Home Page

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -4,17 +4,18 @@ import IntroButton from '../../components/IntroButton';
 import './Home.css';
 
 import logoName from '../../assets/intro-logo-name.png';
-import qubie from '../../assets/Qubie-intro.svg';
+import {ReactComponent as Qubie} from '../../assets/Qubie-intro.svg';
+import footer from '../../assets/intro-lower.png';
 
 class Home extends React.Component {
   render() {
     return (
       <div className="home">
         <div className="container-fluid">
-          <div className="row">
+          <div className="row intro">
             <div className="intro-1 col-lg-7">
-              <img src={qubie} alt="qubie" />
-            </div>
+              <Qubie className="svg-qubie" />
+            </div> 
             <div className="intro-2 col-lg-5">
               <img src={logoName} alt="QuBE Logo" />
               <div className="d-flex justify-content-center">
@@ -39,18 +40,69 @@ class Home extends React.Component {
               </div>
             </div>
           </div>
-          <div className="wave row">
-            <div className="col-lg-5">
-              <span>
-              เทคโนโลยี  ควอนตัม สามารถทำให้เราสร้างควอนตัมคอมพิวเตอร์
-ที่มีประสิทธิภาพในการประมวลผลเหนือคอมพิวเตอร์ในปัจจุบันได้หลายเท่าตัว
-โดยสามารถนำไปใช้งานในด้านต่างๆ ไม่ว่าจะเป็นการถอดรหัสที่รวดเร็ว 
-การพัฒนา AI ที่มีประสิทธิภาพรวมถึงการพยากรณ์ที่แม่นยำ
-              </span>
-            </div> 
+          <div className="row wave">
+            <div className="row subrow">
+              <div className="col-lg-1"></div>
+              <div className="col-lg-4 why">
+                <h2>ทำไมต้องควอนตัม</h2>
+                <h3>ทำไมต้องควอนตัม</h3>
+                <p>
+                เทคโนโลยี  ควอนตัม สามารถทำให้เราสร้างควอนตัมคอมพิวเตอร์
+                ที่มีประสิทธิภาพในการประมวลผลเหนือคอมพิวเตอร์ในปัจจุบันได้หลายเท่าตัว
+                โดยสามารถนำไปใช้งานในด้านต่างๆ ไม่ว่าจะเป็นการถอดรหัสที่รวดเร็ว 
+                การพัฒนา AI ที่มีประสิทธิภาพรวมถึงการพยากรณ์ที่แม่นยำ   
+                </p>
+              </div>
+            </div>
+            <div className="row subrow justify-content-end">
+              <div className="col-lg-5"></div>
+              <div className="col-lg-7 what">
+                <h2>Quantum Computing คืออะไร</h2>
+                <h3>Quantum Computing คืออะไร</h3>
+                  <p>
+                    ระบบคอมพิวเตอร์ที่ทำงานโดยใช้คุณสมบัติพิเศษของอะตอม
+                  แทนการทำงานบนแผงวงจร ด้วยคุณสมบัติพิเศษเหล่านั้น
+                  เราจึงสามารถพัฒนาคอมพิวเตอร์ที่มีประสิทธิภาพมากยิ่งขึ้น
+                  </p>
+              </div>
+            </div>
+          </div>
+          <div className="row feature justify-content-center">
+            <div className="col-5 align-self-center">
+              <div className="head">
+                <h1>QuBE มีอะไรบ้าง</h1>
+              </div>
+              <div className="feature-box feature-explore">
+                <h2>การผจญภัยในโลกควอนตัม</h2>
+                <p>
+                  การผจญภัยไปกับเหล่านางฟ้าในดินแดนอันแสนพิศวง 
+                  พร้อมกันกับการเรียนรู้เนื้อหาควอนตัมคอมพิวเตอร์
+                  ผ่านบทเรียนอันแสนสนุก
+                </p>
+                <IntroButton link="/explore" name="สำรวจโลกควอนตัม" />
+              </div>
+              <div className="feature-box feature-playground">
+                <h2>สนามฝึกซ้อมควอนตัม</h2>
+                <p>
+                  การฝึกซ้อมกำลังให้แข่งแกร่งผ่านการต่อวงจรจำลอง
+                  ควอนตัมก่อนการต่อสู้กับเจ้ามังกรตัวร้าย
+                </p>
+                <IntroButton link="/playground" name="สำรวจโลกควอนตัม" />
+              </div>
+              <div className="feature-box feature-challenge">
+                <h2>ท้าทายกับควอนตัม</h2>
+                <p>
+                  ร่วมต่อสู้กับเจ้ามังกรตัวร้ายด้วยบททดสอบความรู้ 
+                  ความเข้าใจควอนตันคอมพิวเตอร์กับเหล่านางฟ้าทั้งหลาย
+                </p>
+                <IntroButton link="/challenge" name="สำรวจโลกควอนตัม" />
+              </div>
+            </div>
           </div>
         </div>
-        <LoginButton />
+        <footer className="footer ml-0 mr-0">
+            <img src={footer} alt="footer"/>
+        </footer>
       </div>
 
     );
