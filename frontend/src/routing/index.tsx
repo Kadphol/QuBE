f7feba21934ca6c1119bc137b742c97b53b81d7d
Kadphol <kadphol.len@outlook.co.th>
2020-11-09 18:07:07 +0700

    fix switch div

--- frontend/src/routing/index.tsx
+++ frontend/src/routing/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import Switch from 'react-bootstrap/esm/Switch';
 import { Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
 
 export default function Routes() {
   return (
-    <Switch>
+    <>
       <Route path="/" exact component={ Home } />
       <Route path="/explore" />
       <Route path="/playground" />
       <Route path="/challenge" />
-    </Switch>
+    </>
   )
 }
