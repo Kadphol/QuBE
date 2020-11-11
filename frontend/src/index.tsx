3f9a2e38104307cabc36a3090194a8f58dc599fb
DESKTOP-J1EOFF5\surface <tenten25420@gmail.com>
2020-11-11 11:30:15 +0700

    Merging from Feature

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,16 +3,15 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './pages/App';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter } from 'react-router-dom';
+
 
 ReactDOM.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
