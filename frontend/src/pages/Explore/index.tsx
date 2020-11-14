dfe2229230746b77d359d7b08f9dc93dfce169dc
DESKTOP-J1EOFF5\surface <tenten25420@gmail.com>
2020-11-14 22:01:10 +0700

    Add sound component

--- frontend/src/pages/Explore/index.tsx
+++ frontend/src/pages/Explore/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import intro from '../../assets/intro.png';
+import Music from '../../components/sound/Music'
+
 class Explore extends React.Component {
   render() {
     return (
       <div>
         <img src={intro} alt="intro"/>
+        <Music url="https://www.mboxdrive.com/bgm.mp3"/>
       </div>
     );
   }
