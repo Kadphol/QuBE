f4bf29fb0a4dd5826d2c432791807323bd69ad23
Kadphol <kadphol.len@outlook.co.th>
2020-11-23 14:04:47 +0700

    Add progressbar content

--- frontend/src/pages/Explore/index.tsx
+++ frontend/src/pages/Explore/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import intro from '../../assets/intro.png';
 import Music from '../../components/sound/Music'
+import Progressbar from '../../components/Progressbar';
+import './Explore.css';
 
 class Explore extends React.Component {
   render() {
     return (
-      <div>
+      <div className="explore">
+        <Progressbar />
         <img src={intro} alt="intro"/>
         <Music url="https://www.mboxdrive.com/bgm.mp3"/>
       </div>
