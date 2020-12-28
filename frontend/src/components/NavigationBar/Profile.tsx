853ee51976b3463dae597dfb3a3e10c4c20e2842
DESKTOP-J1EOFF5\surface <tenten25420@gmail.com>
2020-12-28 20:47:08 +0700

    add props

--- frontend/src/components/NavigationBar/Profile.tsx
+++ frontend/src/components/NavigationBar/Profile.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import { NavDropdown } from 'react-bootstrap';
-import axios, {AxiosInstance,AxiosRequestConfig} from 'axios'
 
 import './Profile.css';
 import {Iuser} from '../../type.modal'
-
-const config: AxiosRequestConfig = {
-    headers: {'Content-Type': 'application/json'},
-    withCredentials: true
-  }
-const instance: AxiosInstance = axios.create(config);
+import axios from '../../axiosconfig'
 
 class Profile extends React.Component<Iuser> {
 
     constructor(props:Iuser) {
         super(props);
     }
-    
+
     save(){
-        instance.post('http://localhost/updateExplore',{updateid:1})
-    }
+        axios.put('http://localhost/updateInfo',{chapter:1,unit:2,star:3,score:1000})
+        window.location.reload();
+    } 
 
     render() {
     const profileIcon = (<img className="circleBorder" src={this.props.image} width="30" height="30"></img>);
@@ -27,6 +22,7 @@ class Profile extends React.Component<Iuser> {
       <React.Fragment>
             <NavDropdown className="ml-auto" title={profileIcon} id="basic-nav-dropdown">
                 <NavDropdown.Item className="ml-auto" >{this.props.name}</NavDropdown.Item>
+                <NavDropdown.Item className="ml-auto" >Score: {this.props.highscore}</NavDropdown.Item>
                 <NavDropdown.Item className="ml-auto" onClick={this.save}>Save</NavDropdown.Item>
                 <NavDropdown.Item className="ml-auto" href="http://localhost:80/logout">Logout</NavDropdown.Item>
             </NavDropdown>  
