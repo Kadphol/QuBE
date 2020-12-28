e22ff0604f0b9d2c23e1b5686775c02aba2558e9
DESKTOP-J1EOFF5\surface <tenten25420@gmail.com>
2020-12-28 17:43:56 +0700

    simulator

---
+++ backend/simulator.js
@@ -0,0 +1,33 @@
+const API_TOKEN = '78ff82ce42b3e26abb9479f9582835b1efaa84d96ecc354f793d20aae655def2abf621ba067eaceef687a625a173837703d3c69fa95c0beeae46ae1870e4393a'
+const qiskit = require('qiskit');
+const cloud = new qiskit.Cloud();
+const { Circuit, Gate } = require('@qiskit/sim');
+
+module.exports = (app) => {
+    app.post('/sim', (req, res) => {
+        
+        data = req.body
+        
+        const circuit = Circuit.createCircuit(data.n);
+
+        // data.circuit.map()
+        circuit.add(Gate.h, 0, 0);
+        circuit.add(Gate.cx, 1, [0,1]);
+        circuit.print();
+
+        const input = [0,0];
+
+        console.log('\nInput:');
+        console.log(input);
+
+        console.log('\nRunning the circuit now ...');
+        circuit.run(input);
+
+        console.log('\nDone, internal state:');
+        console.log(circuit.state);
+
+        console.log('\nInternal state (as string):');
+        console.log(circuit.stateToString());
+        res.send(circuit.stateToString())
+    });
+}
