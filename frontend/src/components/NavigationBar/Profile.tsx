c2aad614a2d481d2393cefa5b049e5ca2d3f234b
Kadphol <kadphol.len@outlook.co.th>
2021-01-03 15:16:27 +0700

    Fixing warning

--- frontend/src/components/NavigationBar/Profile.tsx
+++ frontend/src/components/NavigationBar/Profile.tsx
@@ -6,15 +6,15 @@ import {Iuser} from '../../type.modal'
 import axios from '../../axiosconfig'
 
 class Profile extends React.Component<Iuser> {
-
-    constructor(props:Iuser) {
-        super(props);
-    }
-
+    
     save(){
         axios.put('http://localhost/updateInfo',{chapter:1,unit:2,star:3,score:1000})
         window.location.reload();
     } 
+
+    logout() {
+        window.location.reload();
+    }
 
     render() {
     const profileIcon = (<img className="circleBorder" src={this.props.image} width="30" height="30" alt="profile"></img>);
@@ -24,7 +24,7 @@ class Profile extends React.Component<Iuser> {
                 <NavDropdown.Item className="ml-auto" >{this.props.name}</NavDropdown.Item>
                 <NavDropdown.Item className="ml-auto" >Score: {this.props.highscore}</NavDropdown.Item>
                 <NavDropdown.Item className="ml-auto" onClick={this.save}>Save</NavDropdown.Item>
-                <NavDropdown.Item className="ml-auto" href="http://localhost:80/logout">Logout</NavDropdown.Item>
+                <NavDropdown.Item className="ml-auto" href="http://localhost:80/logout" onClick={this.logout}>Logout</NavDropdown.Item>
             </NavDropdown>  
       </React.Fragment>
     );
