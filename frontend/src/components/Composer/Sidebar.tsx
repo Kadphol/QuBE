bc7f38dd622736b742320861cc29c7ff92f5d03a
DESKTOP-J1EOFF5\surface <tenten25420@gmail.com>
2021-01-09 22:25:05 +0700

    add sidebar

---
+++ frontend/src/components/Composer/Sidebar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Sidebar.scoped.css'
+
+class Sidebar extends React.Component {
+
+    render() {
+        return (
+            <div className="Sidebar">
+                <div className="Menu">
+                    คู่มือ
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Sidebar;
