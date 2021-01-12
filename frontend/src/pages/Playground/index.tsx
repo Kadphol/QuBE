00bd04a1cc926e696da94a277cad538c202a4e57
DESKTOP-J1EOFF5\surface <tenten25420@gmail.com>
2021-01-12 20:31:13 +0700

    clear unused component

--- frontend/src/pages/Playground/index.tsx
+++ frontend/src/pages/Playground/index.tsx
@@ -1,41 +1,13 @@
 import React from 'react';
-import x from '../../assets/playground/x.png';
-import h from '../../assets/playground/h.png';
-import m from '../../assets/playground/m.png';
-import empty from '../../assets/playground/line.png';
-import q0 from '../../assets/playground/0.png';
-// import q1 from '../../assets/playground/1.png';
-import axios from '../../axiosconfig'
-import { Row } from 'react-bootstrap';
 import Composer from '../../components/Composer'
 import Sidebar from '../../components/Composer/Sidebar'
+import './Playground.scoped.css'
 
-const dict: { [id: string] : string; } = {'x':x,'h':h,'empty':empty};
-
-interface IProps {
-}
-
-interface IState {
-  listResult: string;
-  active: string;
-  n: number;
-  ccimg: string[][];
-  cc: string[]
-}
-
-class Playground extends React.Component <IProps,IState>{
-
-    state = {
-        listResult: '',
-        active: 'empty',
-        n:2,
-        ccimg: [['empty'],['empty']],
-        cc: Array()
-    }
+class Playground extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="flex">
                 <Sidebar/>
                 <Composer/>
             </div>
