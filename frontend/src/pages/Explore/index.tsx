2da2c5b66c13a949d09ed8c81c2f10a29a034927
DESKTOP-J1EOFF5\surface <tenten25420@gmail.com>
2021-01-19 03:27:23 +0700

    Add wrapper and component

--- frontend/src/pages/Explore/index.tsx
+++ frontend/src/pages/Explore/index.tsx
@@ -1,42 +1,60 @@
 import React from 'react';
 import { BrowserRouter as Router,Route, Switch, useRouteMatch } from 'react-router-dom';
 
-import Music from '../../components/sound/Music'
+import Music from '../../components/Sound/Music'
 import Progressbar from '../../components/Progressbar';
 import DialogBox from '../../components/DialogBox';
 import Map from './Map';
 import MenuMap from './MenuMap'
 import './Explore.css';
 import { userContext } from '../../context/userContext';
+import styled from 'styled-components'
 
+const Main = styled.div`
+  position: relative;
+  margin: auto;
+  overflow: hidden;
+  // border: solid violet;
+  * {
+    // border: solid green;
+  }
+`
 
+const MapWrapper = styled.div`
+  position: relative;
+  border: solid gold;
+  * {
+    margin: auto;
+  }
+`
 
 function Explore() {
   
   const { path } = useRouteMatch();
   return (
-    <div className="explore">
+    <Main>
       <userContext.Consumer>
         {
           (user) => <Progressbar {...user}/>
         }
       </userContext.Consumer>
-      <Map>
+      <MapWrapper>
+        <Map>
         <Router>
           <Switch>
             <Route exact path = {path} component = { MenuMap }/>
-            <Route path = {`${path}/chapter-1`} />
+            <Route path = {`${path}/chapter-1`} component = { DialogBox } />
             <Route path = {`${path}/chapter-2`} />
             <Route path = {`${path}/chapter-3`} />
             <Route path = {`${path}/chapter-4`} />
             <Route path = {`${path}/chapter-5`} />
           </Switch>
         </Router>
-        <DialogBox />
+        {/* <DialogBox /> */}
       </Map>
-      
+      </MapWrapper>
       <Music url="https://www.mboxdrive.com/bgm.mp3"/>
-    </div>
+    </Main>
   );
 }
 
