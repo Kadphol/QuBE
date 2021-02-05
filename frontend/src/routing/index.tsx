cd1149bb8eff91651e53c4e3ee532a6270b47c3e
DESKTOP-J1EOFF5\surface <tenten25420@gmail.com>
2021-02-05 14:53:37 +0700

    restrict page

--- frontend/src/routing/index.tsx
+++ frontend/src/routing/index.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React,{useState} from 'react';
+import { Route, useHistory } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import Explore from '../pages/Explore';
 import Playground from '../pages/Playground';
 import Challenge from '../pages/Challenge'
+import {userContext} from '../context/userContext'
+import LoginModal from '../components/LoginModal'
 
 export default function Routes() {
+
+  const history = useHistory()
+
   return (
-    <>
+    <userContext.Consumer>
+      {({user})=>
+      <React.Fragment>
       <Route path="/" exact component={ Home } />
-      <Route path="/explore" component={ Explore }/>
-      <Route path="/playground" exact component={ Playground }/>
-      <Route path="/challenge" component={ Challenge }/>
-    </>
+      <Route path="/explore"    render={()=>user.loginStatus
+                                          ?<Explore/>
+                                          :<LoginModal show 
+                                            onHide={()=>{history.goBack()}}/>}/>
+      <Route path="/playground" render={()=>user.loginStatus
+                                          ?<Playground/>
+                                          :<LoginModal show 
+                                            onHide={()=>{history.goBack()}}/>}/>
+      <Route path="/challenge"  render={()=>user.loginStatus
+                                          ?<Challenge/>
+                                          :<LoginModal show 
+                                            onHide={()=>{history.goBack()}}/>}/>
+      </React.Fragment>
+      }
+    </userContext.Consumer>
   )
 }
