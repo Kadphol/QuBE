36a16f447bf75d2b7249f29358e824754838ff0f
DESKTOP-J1EOFF5\surface <tenten25420@gmail.com>
2021-02-05 14:56:47 +0700

    add click sound

--- frontend/src/components/Question/index.tsx
+++ frontend/src/components/Question/index.tsx
@@ -3,6 +3,11 @@ import styled from 'styled-components'
 import bar from '../../assets/explore/chapter1/quiz/bar.png'
 import barWrong from '../../assets/explore/chapter1/quiz/barWrong.png'
 import barCorrect from '../../assets/explore/chapter1/quiz/barCorrect.png'
+const sfxClick = require('../../assets/sound/sfx_click.mp3').default
+const sfxCorrect = require('../../assets/sound/sfx_correct.mp3').default
+const sfxWrong = require('../../assets/sound/sfx_wrong.mp3').default
+
+
 
 const Main = styled.div`
     height: 415px;
@@ -17,7 +22,7 @@ const Main = styled.div`
     }
     p {
         font-family: 'Kanit',sans-serif;
-        font-size: 35px;
+        font-size: 20px;
         cursor: pointer;
         position: absolute;
         left: 50%;
@@ -37,11 +42,17 @@ class Choice extends React.Component<IProps> {
         barShow: [bar, bar, bar]
     }
 
+    click = new Audio(sfxClick)
+    correct = new Audio(sfxCorrect)
+    wrong = new Audio(sfxWrong)
+
     select = (selectItem) => {
         let valid = selectItem === this.props.solution
         if (this.state.barShow[selectItem] == bar) {
             let barShow = this.state.barShow
             barShow[selectItem] = valid ? barCorrect : barWrong
+            let sfx = valid ? this.correct : this.wrong
+            sfx.play()
             this.setState({ barShow: barShow })
             this.props.answerValidate(valid)
         }
@@ -54,15 +65,21 @@ class Choice extends React.Component<IProps> {
         return (
             <React.Fragment>
                 <Main>
-                    <div onClick={() => this.select(0)}>
+                    <div 
+                    onMouseDown={()=>this.click.play()}
+                    onClick={() => this.select(0)}>
                         <img src={this.state.barShow[0]} style={{ top: '-30px' }} />
                         <p style={{ top: '90px' }}>{this.props.choices[0]}</p>
                     </div>
-                    <div onClick={() => this.select(1)}>
+                    <div 
+                    onMouseDown={()=>this.click.play()}
+                    onClick={() => this.select(1)}>
                         <img src={this.state.barShow[1]} style={{ top: '100px' }} />
                         <p style={{ top: '220px' }}>{this.props.choices[1]}</p>
                     </div>
-                    <div onClick={() => this.select(2)}>
+                    <div 
+                    onMouseDown={()=>this.click.play()}
+                    onClick={() => this.select(2)}>
                         <img src={this.state.barShow[2]} style={{ top: '230px' }} />
                         <p style={{ top: '350px' }}>{this.props.choices[2]}</p>
                     </div>
