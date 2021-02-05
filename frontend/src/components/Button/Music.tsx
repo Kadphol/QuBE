b844192f2a0d91a1d5d1a4cad5d237e10544ec38
DESKTOP-J1EOFF5\surface <tenten25420@gmail.com>
2021-02-05 14:55:02 +0700

    add click sound to button

--- frontend/src/components/Button/Music.tsx
+++ frontend/src/components/Button/Music.tsx
@@ -1,39 +1,49 @@
-import React from 'react';
-import soundon from '../../assets/sound/soundIcon.png';
-import soundoff from '../../assets/sound/soundIcon.png';
+import React,{ useState } from 'react';
+import soundon from '../../assets/sound/soundOn.png';
+import soundoff from '../../assets/sound/soundOff.png';
 import './Music.scoped.css'
+const sfxClick = require('../../assets/sound/sfx_click.mp3').default
 
-class Music extends React.Component <{url:string}>{
-
-    state = {
-      play: false
-    }
-    audio = new Audio(this.props.url);
-  
-    componentDidMount() {
-        this.setState({ play: true })
-        this.audio.volume = 0.1
-        this.audio.play()
-    }
-  
-    componentWillUnmount() {
-        this.setState({ play: false})
-        this.audio.pause()
-    }
-  
-    togglePlay = () => {
-      this.setState({ play: !this.state.play }, () => {
-        this.state.play ? this.audio.play() : this.audio.pause();
-      });
-    }
-  
-    render() {
-      return (
-        <div>
-          <img className="Button" src={this.state.play ? soundon : soundoff} onClick={this.togglePlay} alt="On/off BGM"/>
-        </div>
-      );
-    }
+class Music extends React.Component <{url:string}> {
+  state = {
+    play: true
   }
-  
-  export default Music;
+  audio = new Audio(this.props.url)
+  click = new Audio(sfxClick)
+
+  componentDidMount() {
+    this.audio.addEventListener('ended', () => this.setState({ play: true }));
+    this.audio.volume = 0.4
+    this.audio.loop = true
+    this.audio.play()
+  }
+
+  componentWillUnmount() {
+    this.audio.removeEventListener('ended', () => this.setState({ play: false }));  
+    this.audio.pause()
+  }
+
+  togglePlay = () => {
+    this.setState({ play: !this.state.play }, () => {
+      this.state.play ? this.audio.play() : this.audio.pause();
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <img src={this.state.play?soundon:soundoff} 
+          style={{
+            width: '50px',
+            position: 'absolute',
+            right: '20px',
+            bottom: '15px',
+            cursor: 'pointer'}}
+          onMouseDown={()=>this.click.play()}
+          onClick={this.togglePlay} />
+      </div>
+    );
+  }
+}
+
+export default Music;
--- frontend/src/components/Button/skip.tsx
+++ frontend/src/components/Button/skip.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import backIcon from '../../assets/explore/backIcon.png'
+import skipIcon from '../../assets/explore/skipIcon.png'
 import {Redirect,Link} from 'react-router-dom'
 const sfxClick = require('../../assets/sound/sfx_click.mp3').default
 
-export default function back({path}) {
+export default function skip({path}) {
 
     const click = new Audio(sfxClick)
     
     return (
         <Link to={path} >
-            <img src={backIcon} onMouseDown={()=>click.play()} 
+            <img src={skipIcon} onMouseDown={()=>click.play()} 
             style={{ cursor: 'pointer', position: 'absolute', top: '20px', right: '20px' }} />
         </Link>
     )
