ad8b4abedb21b9509e03972343cbe972a9f2329b
Tenma45 <tenten25420@gmail.com>
2021-05-18 19:00:32 +0700

    update challenge background

--- frontend/src/pages/Challenge/Howtoplay/Frame.tsx
+++ frontend/src/pages/Challenge/Howtoplay/Frame.tsx
@@ -5,8 +5,9 @@ import Qubie from '@assets/explore/Qubie.png'
 import king from '@assets/challenge/king.png'
 import step1 from '@assets/challenge/step1.png'
 import step2 from '@assets/challenge/step2.png'
-import far from '@assets/explore/chapter1/backgroundFar.png';
-import close from '@assets/explore/chapter1/backgroundClose.png';
+import step3 from '@assets/challenge/step3.png'
+import step4 from '@assets/challenge/step4.png'
+import far from '@assets/challenge/backgroundFar.png';
 
 const sfxClick = require('@assets/sound/sfx_click.mp3').default
 
@@ -38,7 +39,7 @@ class F0 extends React.Component<any> {
 
     render() {
         return (<Main>
-            <Content >
+            <Content>
                     <Fly>
                         <img src={king} style={{ position: 'absolute', left: '400px', top: '150px', height: '400px' }} />
                     </Fly>
@@ -51,7 +52,7 @@ class F1 extends React.Component {
 
     render() {
         return (<Main>
-            <Content style={{ backgroundImage: `url(${far})` }}>
+            <Content>
                         <img src={step1} style={{ position: 'absolute', left: '180px', top: '50px', height: '500px' }} />
             </Content>
         </Main>);
@@ -61,11 +62,31 @@ class F1 extends React.Component {
 class F2 extends React.Component {
     render() {
         return (<Main>
-            <Content style={{ backgroundImage: `url(${far})` }}>
+            <Content>
                 <img src={step2} style={{ position: 'absolute', left: '180px', top: '50px', height: '500px' }} />
             </Content>
         </Main>);
     }
 } 
 
-export { F0, F1, F2, }
+class F3 extends React.Component {
+    render() {
+        return (<Main>
+            <Content>
+                <img src={step3} style={{ position: 'absolute', left: '180px', top: '50px', height: '500px' }} />
+            </Content>
+        </Main>);
+    }
+} 
+
+class F4 extends React.Component {
+    render() {
+        return (<Main>
+            <Content>
+                <img src={step4} style={{ position: 'absolute', left: '180px', top: '50px', height: '500px' }} />
+            </Content>
+        </Main>);
+    }
+} 
+
+export { F0, F1, F2, F3, F4}
