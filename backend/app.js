ae2e61183c23f59638d85e30999e3a2c951e1d3c
Kadphol <kadphol.len@outlook.co.th>
2022-04-01 21:57:35 +0700

    Add compress and logger with prod static serve

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const config = require('./config/config');
 const session = require('express-session');
+const MemoryStore = requier('memorystore')(session);
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
+const morgan = require('morgan');
+const path = require('path');
 
 var app = express();
 app.listen(config.APP_PORT);
-//console.log(config.ENDPOINT.URL);
 
+app.use(compression());
+app.use(morgan('tiny'));
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', config.ENDPOINT.URL);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
@@ -16,10 +21,27 @@ app.use(function(req, res, next) {
 }); 
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser())
-app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }))
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(session({ 
+  secret: 'keyboard cat', 
+  store: new MemoryStore({
+    checkPeriod: 86400000
+  }),
+  resave: true, 
+  saveUninitialized: true 
+}));
 
 require('./passport.js')(app)  
 require('./route.js')(app)
 require('./simulator.js')(app)
+
+if(process.env.NODE_ENV === 'production') {
+  //Serve any static files
+  app.use(express.static(path.join(__dirname,'../frontend/build')));
+
+  //Handle React routing, return all request to React ap
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+  });
+}
