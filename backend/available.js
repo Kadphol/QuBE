932920e79fc37ec6d2812701daefcf75bccbdecd
Kadphol <kadphol.len@outlook.co.th>
2022-04-15 14:53:37 +0700

    Add handle error for available api

--- backend/available.js
+++ backend/available.js
@@ -2,8 +2,13 @@ const router = require('express').Router();
 var available = 1;
 
 router.post('/', (req, res) => {
-    available = req.body.available
-    return res.send(`OK ${req.body.available}`)
+    if(req.body.available && req.body.available != 0) {
+        available = req.body.available
+        return res.send(`OK ${req.body.available}`)
+    } else {
+        return res.send(`Error`).statusCode(400);
+    }
+
 });
 
 router.get('/', (req, res) => {
