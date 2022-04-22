1bc93800cfad6c85cdcc1395c30621fc36a35707
Kadphol <kadphol.len@outlook.co.th>
2022-04-22 16:15:49 +0700

    Add regexp for username and password

--- frontend/src/components/LoginModal/index.tsx
+++ frontend/src/components/LoginModal/index.tsx
@@ -9,6 +9,8 @@ import ENDPOINT from '@config/endpoint'
 export default function LoginModal(props) {
 
   const history = useHistory();
+  const regexp = /^[a-zA-Z0-9!@#$%^&*)(+=._-]+$/g;
+  const namereg = /[^a-zA-Z0-9]/g;
 
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
@@ -96,6 +98,8 @@ export default function LoginModal(props) {
       setErrorNameMsg('กรุณากรอกชื่อผู้ใช้งาน');
     } else if(input.length < 4) {
       setErrorNameMsg('ชื่อผู้ใช้ต้องมีความยาวมากกว่า 4 ตัวอักษร');
+    } else if(namereg.test(input)){ 
+      setErrorNameMsg('ชื่อผู้ใช้งานต้องมีเฉพาะตัวอักษรภาษาอังกฤษและตัวเลขเท่านั้น');
     } else {
       setErrorNameMsg('');
     }
@@ -107,6 +111,8 @@ export default function LoginModal(props) {
       setErrorPasswordMsg('กรุณากรอกรหัสผ่าน');
     } else if(input.length < 4) {
       setErrorPasswordMsg('รหัสผ่านต้องมีความยาวมากกว่า 4 ตัวอักษร');
+    } else if(regexp.test(input)){ 
+      setErrorPasswordMsg('รหัสผ่านมีอักขระพิเศษที่ไม่สามารถใช้ได้อยู่');
     } else {
       setErrorPasswordMsg('');
     }
