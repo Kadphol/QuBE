ef67957d497684f2171d65deddcfa99b1f9695d9
Kadphol <kadphol.len@outlook.co.th>
2023-03-08 22:11:35 +0700

    fix(app): add basename on router

--- frontend/src/pages/App/index.tsx
+++ frontend/src/pages/App/index.tsx
@@ -1,53 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
 
-import NavigationBar from '@components/NavigationBar';
-import Routes from '@route/index'
+import NavigationBar from "@components/NavigationBar";
+import Routes from "@route/index";
 
-import '@styles/App.css';
-import { Iuser } from '@src/type.modal';
+import "@styles/App.css";
+import { Iuser } from "@src/type.modal";
 
-import axios from '@config/axiosconfig';
-import { userContext } from '@context/userContext';
+import axios from "@config/axiosconfig";
+import { userContext } from "@context/userContext";
 
 function App() {
-
   const [user, setUser] = useState<Iuser>({
-    loginStatus: false
+    loginStatus: false,
   });
 
-  const providerValue = {user,setUser}
+  const providerValue = { user, setUser };
 
-  useEffect(()=>{
-    axios.get(`/fetch`)
-    .then(res => {
-      if (res.data.info !== undefined) {
-        setUser( () => ({
-          loginStatus: true,
-          type: res.data.type,
-          name: res.data.name,
-          image: res.data.image,
-          chapter: res.data.info.chapter,
-          unit: res.data.info.unit,
-          star: res.data.info.star,
-          highscore: res.data.info.highscore
-        }));
-      }
-    })
-    .catch(error =>{
-      if(error.response.status === 401) {
-        console.log("unauthorized");
-      } else {
-        console.error(error);
-      }
-    })
-  },[])
+  useEffect(() => {
+    axios
+      .get(`/fetch`)
+      .then((res) => {
+        if (res.data.info !== undefined) {
+          setUser(() => ({
+            loginStatus: true,
+            type: res.data.type,
+            name: res.data.name,
+            image: res.data.image,
+            chapter: res.data.info.chapter,
+            unit: res.data.info.unit,
+            star: res.data.info.star,
+            highscore: res.data.info.highscore,
+          }));
+        }
+      })
+      .catch((error) => {
+        if (error.response.status === 401) {
+          console.log("unauthorized");
+        } else {
+          console.error(error);
+        }
+      });
+  }, []);
 
   return (
     <div className="App">
       <userContext.Provider value={providerValue}>
-        <BrowserRouter>
-            <NavigationBar />
+        <BrowserRouter basename="/game">
+          <NavigationBar />
           <Routes />
         </BrowserRouter>
       </userContext.Provider>
