42d3f63988c3eac0f968c2ff4a79e20e69d6b28c
Kadphol <kadphol.len@outlook.co.th>
2023-03-08 21:28:41 +0700

    fix(config): remove port on prod

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,20 +1,22 @@
-require('dotenv').config({ path: __dirname + '../.env' });
+require("dotenv").config({ path: __dirname + "../.env" });
 
 const prod = {
-  URL: `${process.env.CLIENT_URL}:${process.env.APP_PORT}`,
-  FRONTEND_URL: `${process.env.CLIENT_URL}:${process.env.APP_PORT}`
+  URL: `${process.env.CLIENT_URL}`,
+  FRONTEND_URL: `${process.env.CLIENT_URL}`,
 };
 
 const dev = {
-  URL: 'http://localhost',
-  URL_404: 'http://localhost/404',
-  FRONTEND_URL: 'http://localhost:3000'
+  URL: "http://localhost",
+  URL_404: "http://localhost/404",
+  FRONTEND_URL: "http://localhost:3000",
 };
 
 module.exports = {
-  DB: process.env.MONGO_URI ? process.env.MONGO_URI : 'mongodb://localhost:27017/qube',
-  APP_PORT: process.env.APP_PORT ? process.env.APP_PORT : '80',
-  ENDPOINT: process.env.NODE_ENV === 'production' ? prod : dev,
+  DB: process.env.MONGO_URI
+    ? process.env.MONGO_URI
+    : "mongodb://localhost:27017/qube",
+  APP_PORT: process.env.APP_PORT ? process.env.APP_PORT : "80",
+  ENDPOINT: process.env.NODE_ENV === "production" ? prod : dev,
   CLIENT_ID: process.env.CLIENT_ID,
-  CLIENT_SECERT: process.env.CLIENT_SECRET
+  CLIENT_SECERT: process.env.CLIENT_SECRET,
 };
