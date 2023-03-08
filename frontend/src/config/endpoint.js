1e71f672a07a718dbfbf23c9c34c317908493313
Kadphol <kadphol.len@outlook.co.th>
2023-03-08 21:21:12 +0700

    fix(endpoint): fix endpoint for prod

--- frontend/src/config/endpoint.js
+++ frontend/src/config/endpoint.js
@@ -1,9 +1,10 @@
-  const prod = {
-    URL: 'http://10.35.24.191:8080',
-  };
-  
-  const dev = {
-    URL: 'http://localhost:80',
-  };
-  
-  module.exports = process.env.NODE_ENV === 'development' ? dev : prod
+const prod = {
+  // URL: 'http://10.35.24.191:8080',
+  URL: "https://qx.kmutt.ac.th/game/",
+};
+
+const dev = {
+  URL: "http://localhost:80",
+};
+
+module.exports = process.env.NODE_ENV === "development" ? dev : prod;
