b86361a6e87e67b97296ecb43b8eb165d4b0d000
Kadphol <kadphol.len@outlook.co.th>
2023-03-11 01:36:50 +0700

    fix(route): fix router path for explore and challenge

--- frontend/src/pages/Challenge/index.tsx
+++ frontend/src/pages/Challenge/index.tsx
@@ -1,39 +1,61 @@
-import React from 'react';
+import React from 'react'
 import Play from './Play'
 import Leaderboard from './Leaderboard'
 import Menu from './Menu'
 import HowToplay from './Howtoplay'
-import { userContext } from '@context/userContext';
+import { userContext } from '@context/userContext'
 import styled from 'styled-components'
 import scene1 from '@assets/challenge/backgroundFar.png'
-import { BrowserRouter as Router, Route, Switch, useRouteMatch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useRouteMatch,
+} from 'react-router-dom'
 
 const Wrapper = styled.div`
   background-image: url(${scene1});
   height: 720px;
   width: 1240px;
   margin: 60px auto 20px;
-  display:flex;
+  display: flex;
   position: relative;
 `
 export default function Challenge() {
-
-  const { path } = useRouteMatch();
+  const { path } = useRouteMatch()
 
   return (
     <Wrapper>
-      <Router>
-      <userContext.Consumer>
-            { ({user,setUser}) =>
-        <Switch>
-          <Route exact path={path} component={Menu} />
-            <Route path={`${path}/play`} render={() => (<Play user={user} setUser={setUser} />)}/>
-            <Route path={`${path}/leaderboard`} render={() => (<Leaderboard user={user} />)} />
-            <Route path={`${path}/howtoplay`} component={HowToplay}/>
-        </Switch>
-            }
+      <Router basename="/game">
+        <userContext.Consumer>
+          {({ user, setUser }) => (
+            <Switch>
+              <Route
+                exact
+                path={path}
+                component={Menu}
+              />
+              <Route
+                path={`${path}/play`}
+                render={() => (
+                  <Play
+                    user={user}
+                    setUser={setUser}
+                  />
+                )}
+              />
+              <Route
+                path={`${path}/leaderboard`}
+                render={() => <Leaderboard user={user} />}
+              />
+              <Route
+                path={`${path}/howtoplay`}
+                render={() => <HowToplay />}
+              />
+            </Switch>
+          )}
         </userContext.Consumer>
       </Router>
     </Wrapper>
-  );
+  )
 }
--- frontend/src/pages/Explore/index.tsx
+++ frontend/src/pages/Explore/index.tsx
@@ -1,18 +1,23 @@
-import React, { useState } from 'react';
-import { BrowserRouter, BrowserRouter as Router, Route, Switch, useRouteMatch } from 'react-router-dom';
+import React from 'react'
+import {
+  BrowserRouter,
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useRouteMatch,
+} from 'react-router-dom'
 
-import Progressbar from '@components/Progressbar';
-import Map from './Map';
-import MenuChapter from './MenuChapter';
-import './Explore.css';
-import { userContext } from '@context/userContext';
-import styled from 'styled-components';
-import Chapter1 from './Chapter1';
-import Chapter2 from './Chapter2';
-import Chapter3 from './Chapter3';
-import Chapter4 from './Chapter4';
-import Chapter5 from './Chapter5';
-const bgm1 = require('@assets/sound/bgm1.mp3');
+import Progressbar from '@components/Progressbar'
+import Map from './Map'
+import MenuChapter from './MenuChapter'
+import './Explore.css'
+import { userContext } from '@context/userContext'
+import styled from 'styled-components'
+import Chapter1 from './Chapter1'
+import Chapter2 from './Chapter2'
+import Chapter3 from './Chapter3'
+import Chapter4 from './Chapter4'
+import Chapter5 from './Chapter5'
 
 const Main = styled.div`
   height: 100%;
@@ -29,37 +34,52 @@ const MapWrapper = styled.div`
 `
 
 export default function Explore() {
-
-  const { path } = useRouteMatch();
-  const [src,setSrc] = useState(bgm1)
+  const { path } = useRouteMatch()
 
   return (
-
     <Main>
       <userContext.Consumer>
-        {({ user, setUser }) =>
+        {({ user, setUser }) => (
           <React.Fragment>
             <Progressbar {...user} />
             <MapWrapper>
               <Map>
-                <BrowserRouter>
+                <BrowserRouter basename="/game">
                   <Router>
                     <Switch>
-                      <Route exact path={path} render={() => <MenuChapter {...user}/>} />
-                      <Route path={`${path}/chapter-1`} component={Chapter1} />
-                      <Route path={`${path}/chapter-2`} component={Chapter2} />
-                      <Route path={`${path}/chapter-3`} component={Chapter3}/>
-                      <Route path={`${path}/chapter-4`} component={Chapter4}/>
-                      <Route path={`${path}/chapter-5`} component={Chapter5}/>
+                      <Route
+                        exact
+                        path={path}
+                        render={() => <MenuChapter {...user} />}
+                      />
+                      <Route
+                        path={`${path}/chapter-1`}
+                        component={Chapter1}
+                      />
+                      <Route
+                        path={`${path}/chapter-2`}
+                        component={Chapter2}
+                      />
+                      <Route
+                        path={`${path}/chapter-3`}
+                        component={Chapter3}
+                      />
+                      <Route
+                        path={`${path}/chapter-4`}
+                        component={Chapter4}
+                      />
+                      <Route
+                        path={`${path}/chapter-5`}
+                        component={Chapter5}
+                      />
                     </Switch>
                   </Router>
                 </BrowserRouter>
               </Map>
             </MapWrapper>
           </React.Fragment>
-        }
+        )}
       </userContext.Consumer>
     </Main>
-
-  );
+  )
 }
