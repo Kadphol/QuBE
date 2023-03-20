32a633ff91e59d71a5f6c15d6f9533b0ba22fd84
Kadphol <kadphol.len@outlook.co.th>
2023-03-20 21:22:53 +0700

    fix(guest): fix guest login redirect

--- backend/route.js
+++ backend/route.js
@@ -1,9 +1,8 @@
-
 var passport = require('passport')
 const users = require('./db/users.js')
 const explore = require('./db/explore.js')
-const router = require('express').Router();
-const config = require('./config/config');
+const router = require('express').Router()
+const config = require('./config/config')
 
 router.get('/', (req, res) => {
   res.send('Nothing here!')
@@ -12,45 +11,60 @@ router.get('/', (req, res) => {
 router.get('/fetch', (req, res) => {
   if (req.isAuthenticated()) {
     users.fetch(req.user.id, function (err, data) {
-        if (err) return res.status(401).send('no authorized');
-        return res.send(data)
-    }) 
+      if (err) return res.status(401).send('no authorized')
+      return res.send(data)
+    })
   } else {
-    return res.status(401).send('no authorized');
+    return res.status(401).send('no authorized')
   }
-});
+})
 
 router.get('/login', passport.authenticate('facebook'))
-router.get('/login/callback', passport.authenticate('facebook', {
-  successRedirect: config.ENDPOINT.FRONTEND_URL,
-  failureRedirect: config.ENDPOINT.FRONTEND_URL
-}));
+router.get(
+  '/login/callback',
+  passport.authenticate('facebook', {
+    successRedirect: config.ENDPOINT.FRONTEND_URL,
+    failureRedirect: config.ENDPOINT.FRONTEND_URL,
+  })
+)
 
-router.get('/guestlogin', passport.authenticate('dummy', {
-  successRedirect: config.ENDPOINT.FRONTEND_URL,
-  failureMessage: true
-}));
+router.get('/guestlogin', passport.authenticate('dummy'), (req, res) => {
+  return res.redirect(req.headers.referer || '/')
+})
 
-router.post('/register', passport.authenticate('local-register', {
-  successRedirect: config.ENDPOINT.FRONTEND_URL,
-  failureMessage: true
-}));
+router.post(
+  '/register',
+  passport.authenticate('local-register', {
+    successRedirect: config.ENDPOINT.FRONTEND_URL,
+    failureMessage: true,
+  })
+)
 
-router.post('/local-login', passport.authenticate('local-login', {
-  successRedirect: config.ENDPOINT.FRONTEND_URL,
-  failureMessage: true
-}))
+router.post(
+  '/local-login',
+  passport.authenticate('local-login', {
+    successRedirect: config.ENDPOINT.FRONTEND_URL,
+    failureMessage: true,
+  })
+)
 
 router.put('/updateInfo', (req, res) => {
-  users.updateInfo(req.user, req.body.chapter, req.body.unit, req.body.star, req.body.score, () => {
-    return res.send("OK")
-  })
+  users.updateInfo(
+    req.user,
+    req.body.chapter,
+    req.body.unit,
+    req.body.star,
+    req.body.score,
+    () => {
+      return res.send('OK')
+    }
+  )
 })
 
 router.get('/logout', (req, res) => {
   req.session.destroy(function (err) {
     req.logout()
-    return res.redirect(req.headers.referer || '/');
+    return res.redirect(req.headers.referer || '/')
   })
 })
 
@@ -82,4 +96,4 @@ router.get('/removeGuest', (req, res) => {
   res.redirect('/api/getuser')
 })
 
-module.exports = router;
+module.exports = router
